3c112c08710efe9b555a8c6b6560865a5e7aafd5
Kamron Khamrakulov <khamrakulovkamron@gmail.com>
2025-01-10 16:54:28 +0500

    some fixes

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -94,5 +94,17 @@
         line_break.disabled = true;
       };
     };
+
+    alacritty = {
+      enable = true;
+      settings = {
+        env.TERM = "xterm-256color";
+        font = {
+          size = 12;
+        };
+        scrolling.multiplier = 5;
+        selection.save_to_clipboard = true;
+      };
+    };
   };
 }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -130,18 +130,6 @@
       userEmail = "khamrakulovkamron@gmail.com";
     };
 
-    alacritty = {
-      enable = true;
-      settings = {
-        env.TERM = "xterm-256color";
-        font = {
-          size = 12;
-        };
-        scrolling.multiplier = 5;
-        selection.save_to_clipboard = true;
-      };
-    };
-
     bash = {
       enable = true;
       enableCompletion = true;
