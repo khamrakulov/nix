bdac7bba0b7246c41479a0177e2b610dac5132b5
Kamron Khamrakulov <khamrakulovkamron@gmail.com>
2025-01-11 14:10:49 +0500

    some fixes

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = { inherit nixpkgs self home-manager inputs; };
+        specialArgs = { inherit nixpkgs home-manager; };
         modules = [
           ./nixos/configuration.nix
           ./nixos/modules/boot-loader.nix
