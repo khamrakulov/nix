798b00d9f5addb172d67bd626a2be8c742937035
xfeusw <khamrakulovkamron@gmail.com>
2025-02-12 22:40:17 +0500

    small changes

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.xfeusw = import ./home.nix;
+            home-manager.users.xfeusw = import ./home-manager/home.nix;
           }
         ];
       };
