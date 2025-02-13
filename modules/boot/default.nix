50dc74eddc6eb516af874c571a8ab8b5cbc12930
xfeusw <khamrakulovkamron@gmail.com>
2025-02-13 11:53:56 +0500

    switch to systemd-boot

--- modules/boot/default.nix
+++ modules/boot/default.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 {
   imports = [
-    ./grub.nix  # Use GRUB by default
-    # ./systemd-boot.nix  # Uncomment to switch to systemd-boot
+    # ./grub.nix  # Use GRUB by default
+    ./systemd-boot.nix  # Uncomment to switch to systemd-boot
   ];
 
   environment.systemPackages = with pkgs; [
