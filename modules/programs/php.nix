5b90604850cecb86cfea7e07b02f9995dab838e0
xfeusw <khamrakulovkamron@gmail.com>
2025-02-20 13:20:38 +0500

    some changes with phpfpm

--- modules/programs/php.nix
+++ modules/programs/php.nix
@@ -16,5 +16,21 @@
     phpPackages.fileinfo
   ];
 
-  services.phpfpm.enable = true;  # Enable PHP-FPM if needed
+  services.phpfpm.pools.laravel = {
+    user = "nginx";  # Change this if using Apache or another user
+    group = "nginx";
+    phpOptions = ''
+      post_max_size = 50M
+      upload_max_filesize = 50M
+      max_execution_time = 300
+    '';
+    settings = {
+      "listen" = "/run/phpfpm/laravel.sock";
+      "pm" = "dynamic";
+      "pm.max_children" = 10;
+      "pm.start_servers" = 3;
+      "pm.min_spare_servers" = 2;
+      "pm.max_spare_servers" = 5;
+    };
+  };
 }
