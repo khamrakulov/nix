610bf60af8af215d9c7258e6aa3d29f36c81d554
xfeusw <khamrakulovkamron@gmail.com>
2025-02-27 10:38:43 +0500

    minimal boot changes

--- modules/boot/systemd-boot.nix
+++ modules/boot/systemd-boot.nix
@@ -12,7 +12,7 @@
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "${pkgs.systemd}/bin/bootctl update";  # Remove '|| true'
+      ExecStart = "${pkgs.systemd}/bin/bootctl update || exit 0";  # Ignore failures
       RemainAfterExit = true;
     };
   };
