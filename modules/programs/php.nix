9b6389634cd9e7da14e61badcc63bc026fe40a8a
installer <installer@gmail.com>
2025-03-10 19:46:49 +0500

    php optimization

--- modules/programs/php.nix
+++ modules/programs/php.nix
@@ -2,25 +2,7 @@
 
 {
   environment.systemPackages = with pkgs; [
-    php84
-    php84Packages.composer
+    php83
+    php83Packages.composer
   ];
-
-  services.phpfpm.pools.laravel = {
-    user = "xfeusw";  # Change this if using Apache or another user
-    group = "xfeusw";
-    phpOptions = ''
-      post_max_size = 50M
-      upload_max_filesize = 50M
-      max_execution_time = 300
-    '';
-    settings = {
-      "listen" = "/run/phpfpm/laravel.sock";
-      "pm" = "dynamic";
-      "pm.max_children" = 10;
-      "pm.start_servers" = 3;
-      "pm.min_spare_servers" = 2;
-      "pm.max_spare_servers" = 5;
-    };
-  };
 }
